789d754ff50bf53b5c4c9fe1a34d0d12d1bbec37
Wen Cong <S10194723@connect.np.edu.sg>
2020-11-12 09:02:21 +0800

    Initial Commit

---
+++ Telegramme/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Telegramme
+//
+//  Created by Yeo Wen Cong on 12/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
